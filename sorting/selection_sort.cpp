4c719ee4f523e304297bd8bd9d42d3b505ed0cc5
Han Yan <yanhan0121@gmail.com>
2023-12-05 01:17:57 -0500

    12/5/23 add selection sort

--- sorting/selection_sort.cpp
+++ sorting/selection_sort.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include <vector>
 
-
 void selection_sort(std::vector<int>& v) {
-    for (int i = 0; i < v.size(); ++i) {
-        int min = i;
-        for (int j = i + 1; j < v.size(); ++j) {
-            if (v[j] < v[min]) {
-                min = j;
-            }
-        }
-        std::swap(v[i], v[min]);
-    }
+	for (int i = 0; i < v.size(); ++i) {
+		int min = i;
+		for (int j = i + 1; j < v.size(); ++j) {
+			if (v[j] < v[min]) {
+				min = j;
+			}
+		}
+		std::swap(v[i], v[min]);
+	}
+}
+
+int main() {
+	std::cout << "Selection sort" << std::endl;
+	std::vector<int> v = {5, 4, 3, 2, 1};
+	std::cout << "Before sorting: ";
+	for (int i = 0; i < v.size(); ++i) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+	selection_sort(v);
+	std::cout << "After sorting: ";
+	for (int i = 0; i < v.size(); ++i) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
